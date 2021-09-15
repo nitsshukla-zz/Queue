9f4cf960d0ba06c082ed3c2de8ed365d5d49803c
Nitin <nitin1.shukla@ril.com>
2021-09-15 07:52:23 +0530

    Added swagger

--- src/main/java/com/navi/app/QueueApplication.java
+++ src/main/java/com/navi/app/QueueApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.retry.annotation.EnableRetry;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableRetry
+@EnableSwagger2
 public class QueueApplication {
 
 	public static void main(String[] args) {
